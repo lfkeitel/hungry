9d808d78df09a6fdb441e817de8da78045178ab8
Lee Keitel <lee@keitel.xyz>
2023-01-24 09:04:07 -0600

    Show locations as buttons, add history

--- main.js
+++ main.js
@@ -51,27 +51,60 @@ const userLocation = () => {
   return path;
 };
 
+let currentLoc = userLocation();
+
 const randVal = max => Math.floor(Math.random() * max);
 const openToday = schStr => schStr.split(",").some(dayAbbv => dayAbbrvs(dayAbbv) == day)
 const makeChoice = () => document.getElementById("eat").textContent = weightedChoices[randVal(weightedChoices.length)].name;
 
-restaurants[userLocation()].forEach(val => {
-  if (typeof val === "string") {
-    val = { name: val };
+const loadChoices = (currentLoc) => {
+  weightedChoices = [];
+
+  restaurants[currentLoc].forEach(val => {
+    if (typeof val === "string") {
+      val = { name: val };
+    }
+
+    if (!openToday(val.sch ?? "s,m,t,w,r,f,sa")) {
+      return;
+    }
+
+    const weight = val.weight ? val.weight < -4 ? -4 : val.weight : 0;
+    for (let i = 0; i < (5 + weight); i++) {
+      weightedChoices.push(val);
+    }
+  });
+};
+
+document.getElementById("result").addEventListener("click", () => makeChoice());
+document.getElementById("result").addEventListener("mousedown", () => document.body.style.backgroundColor = "rgb(162, 255, 73)");
+document.getElementById("result").addEventListener("touchstart", () => document.body.style.backgroundColor = "rgb(162, 255, 73)");
+document.getElementById("result").addEventListener("mouseup", () => document.body.style.backgroundColor = "lawngreen");
+document.getElementById("result").addEventListener("touchend", () => document.body.style.backgroundColor = "lawngreen");
+window.addEventListener("popstate", (event) => {
+  const r = event.state;
+  if (r !== currentLoc) {
+    currentLoc = r;
   }
+  loadChoices(r);
+  makeChoice();
+})
 
-  if (!openToday(val.sch ?? "s,m,t,w,r,f,sa")) {
-    return;
-  }
+for (const r in restaurants) {
+  const b = document.createElement("button")
+  b.textContent = r;
+  b.classList.add("restButton");
+  b.addEventListener("click", () => {
+    if (r !== currentLoc) {
+      window.history.pushState(r, "", `?${r}`);
+      currentLoc = r;
+    }
+    loadChoices(r);
+    makeChoice();
+  });
 
-  const weight = val.weight ? val.weight < -4 ? -4 : val.weight : 0;
-  for (let i = 0; i < (5 + weight); i++) {
-    weightedChoices.push(val);
-  }
-});
+  document.getElementById("cities").appendChild(b);
+}
 
-window.addEventListener("click", () => makeChoice());
-window.addEventListener("mousedown", () => document.body.style.backgroundColor = "rgb(162, 255, 73)");
-window.addEventListener("mouseup", () => document.body.style.backgroundColor = "lawngreen");
-
+loadChoices(currentLoc);
 makeChoice();
