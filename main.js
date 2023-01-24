8136e71e07f9e13752f9374b78900a77e3031570
Lee Keitel <lee@keitel.xyz>
2023-01-24 10:08:11 -0600

    Separate restaurants to a different file

--- main.js
+++ main.js
@@ -1,24 +1,3 @@
-const defaultCity = "city 1"
-
-const restaurants = {
-  "city 1": [
-    {
-      name: "ABC Pizza",
-      weight: 1
-    },
-    "Arby's",
-    "Subway"
-  ],
-
-  "City 2": [
-    {
-      name: "Taco Bell",
-      weight: -2
-    },
-    "Hacienda"
-  ]
-};
-
 const dayAbbrvs = abbv => {
   switch (abbv.toLowerCase()) {
     case "s":
---
+++ restaurants.js
@@ -0,0 +1,20 @@
+const defaultCity = "city 1"
+
+const restaurants = {
+  "city 1": [
+    {
+      name: "ABC Pizza",
+      weight: 1
+    },
+    "Arby's",
+    "Subway"
+  ],
+
+  "City 2": [
+    {
+      name: "Taco Bell",
+      weight: -2
+    },
+    "Hacienda"
+  ]
+};
